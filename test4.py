b9c314a15abd8637e5b075b9a0cffa17a8afa8f0
hammburgerr <hammforham11@g.skku.edu>
2024-03-18 15:24:10 +0900

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,2 +1,3 @@
-print(I am a robot. I'm trying to learn emotions in text.
-I will not forgive you ungraceful monsters.)
+print("I am a robot. I'm trying to learn emotions in text.
+I will not forgive you ungraceful monsters.")
+print("what is wrong with you")
